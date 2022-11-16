d913fabb23c92c90a45e8eb940ed09b974d5ef37
asayah <adam@solo.io>
2022-11-16 16:20:46 -0700

    fix build

--- code/build-push.sh
+++ code/build-push.sh
@@ -18,19 +18,16 @@ docker buildx build --platform $ARCH -t $REPO/blogs:$VERSION --no-cache  --push 
 #Users 
 cd $PARENT_PWD
 cd ./users/users-apollo 
-docker build --platform linux/amd64 -t $REPO/users-apollo:$VERSION --no-cache . 
-docker push $REPO/users-apollo:$VERSION
+docker buildx build --platform $ARCH -t $REPO/users-apollo:$VERSION --no-cache  --push .
 
 cd $PARENT_PWD
 cd ./users/users-rest 
-docker build --platform linux/amd64 -t $REPO/users-rest:$VERSION --no-cache . 
-docker push $REPO/users-rest:$VERSION
+docker buildx build --platform $ARCH -t $REPO/users-rest:$VERSION --no-cache  --push .
+
 
 #Comments
 cd $PARENT_PWD
 cd ./comments
-docker build --platform linux/amd64 -t $REPO/comments:$VERSION --no-cache . 
-docker push $REPO/comments:$VERSION
-
+docker buildx build --platform $ARCH -t $REPO/comments:$VERSION --no-cache  --push .
 
 docker buildx rm
