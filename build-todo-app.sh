849bc1406ad8645e527ee52f685dcbf4a088236e
Jake Cukjati <jakecukjati@gmail.com>
2023-03-17 13:00:43 -0500

    update to the readme

--- build-todo-app.sh
+++ build-todo-app.sh
@@ -3,14 +3,15 @@
 # build-todo-app.sh ${VERSION}
 
 VERSION=$1
+IMAGE_REPO=$2
 echo $VERSION
 if [[ $VERSION ]]
 then
-    docker build -f ./code/todo-app/Dockerfile -t gcr.io/solo-test-236622/graphql-todo:${VERSION}-amd64 --platform linux/amd64 . 
-    docker build -f ./code/todo-app/Dockerfile -t gcr.io/solo-test-236622/graphql-todo:${VERSION}-arm64 --platform linux/arm64 . 
-    echo "ensure that this is the correct version that you want and then use the following commands to push the images"
-    echo "docker push gcr.io/solo-test-236622/graphql-todo:$VERSION-amd64"
-    echo "docker push gcr.io/solo-test-236622/graphql-todo:$VERSION-arm64"
+    docker build -f ./code/todo-app/Dockerfile -t ${IMAGE_REPO}:${VERSION}-amd64 --platform linux/amd64 . 
+    docker build -f ./code/todo-app/Dockerfile -t ${IMAGE_REPO}:${VERSION}-arm64 --platform linux/arm64 . 
+    echo "created the following images"
+    echo "$IMAGE_REPO:$VERSION-arm64"
+    echo "$IMAGE_REPO:$VERSION-amd64"
 else
     echo "please provide a VERSION"
 fi
